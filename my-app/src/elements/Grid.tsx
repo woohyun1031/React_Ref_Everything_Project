a22c9f971484d744309b81744b087531176c196c
Woohyun <ktkwhms3@gmail.com>
2022-06-12 18:07:58 +0900

    Feat: commentList, Item

--- my-app/src/elements/Grid.tsx
+++ my-app/src/elements/Grid.tsx
@@ -7,16 +7,18 @@ type GridProps = {
 	margin?: string | undefined;
 	bg?: boolean | undefined;
 	children?: any | undefined;
+	border?: boolean;
 };
 
 const Grid = (props: GridProps) => {
-	const { is_flex, width, padding, margin, bg, children } = props;
+	const { is_flex, width, padding, margin, bg, border, children } = props;
 	const styles = {
 		is_flex,
 		width,
 		padding,
 		margin,
 		bg,
+		border,
 	};
 
 	return <GridBox {...styles}>{children}</GridBox>;
@@ -30,13 +32,15 @@ const GridBox = styled.div<{
 	padding?: string;
 	margin?: string;
 	bg?: boolean;
+	border?: boolean;
 }>`
 	width: ${({ width }) => width};
 	height: 100%;
 	box-sizing: border-box;
 	padding: ${({ padding }) => (padding ? padding : '0px')};
 	margin: ${({ margin }) => (margin ? margin : '0px')};
-	background-color: ${({ bg }) => (bg ? 'red' : 'gray')};
+	background-color: ${({ bg }) => (bg ? 'white' : 'gray')};
+	border: ${({ border }) => (border ? '1px black solid' : 'none')};
 	display: ${({ is_flex }) => (is_flex ? 'flex' : 'block')};
 	${({ is_flex }) => (is_flex ? 'align-items: center' : '')};
 	${({ is_flex }) => (is_flex ? 'justify-content: space-between' : '')};
